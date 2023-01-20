b81e5d69faec18ef8dd0fa99445e484ce122dcff
AliciaRogers <122655372+AliciaRogers@users.noreply.github.com>
2023-01-19 22:31:03 -0500

    Create Trapezoid.cpp

---
+++ Trapezoid.cpp
@@ -0,0 +1,13 @@
+#include "Trapezoid.h"
+
+Trapezoid::Trapezoid(float base1, float base2, float height) {
+	this-> base1 = base1;
+	this-> base2 = base2;
+	this-> height = height;
+}
+
+float Trapezoid::calcTrapezoidArea(){
+	area = 0.5 * height * (base1 + base2);
+
+	return area;
+}
